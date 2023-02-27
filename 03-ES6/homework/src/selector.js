cf70dbfa9f8c1b1d079f565cd2681f5471347e45
cristhianleon22 <cristhiancleon@gmail.com>
2023-02-26 22:45:38 -0500

    m2

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -7,9 +7,16 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
-
+  if(matchFunc(startEl)){
+    resultSet.push(startEl);
+  }
   // TU CÓDIGO AQUÍ
-  
+   for (let i = 0; i < startEl.children.length; i++) {
+    var elements = traverseDomAndCollectElements(matchFunc, startEl.children[i]);
+    resultSet =[...resultSet, ...elements];
+   }
+
+   return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +25,16 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if (selector[0]=== '#'){
+    return 'id';
+  }
+  if(selector[0]==='.'){
+    return 'class';
+  }
+  if (selector.split('.').length>1){
+    return 'tag.class';
+  }
+  return 'tag';
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +46,29 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+   matchFunction = function (el){
+    return '#'+el.id === selector;
+   }
   } else if (selectorType === "class") {
+    matchFunction = function(el){
+      var classes = el.classList;
+      for(let i=0;i<classes.length;i++){
+        if('.'+classes[i]===selector) return true;
+      }
+      return false;
+    }
     
   } else if (selectorType === "tag.class") {
+    matchFunction = function(el){
+      var [tagBuscado, classBuscado] = selector.split('.');
+      return matchFunctionMaker(tagBuscado)(el) && matchFunctionMaker('.'+classBuscado)(el);
+    }
     
   } else if (selectorType === "tag") {
-    
+    matchFunction= function(el){
+      return el.tagName.toLowerCase() === selector;
+    }
+  
   }
   return matchFunction;
 };
