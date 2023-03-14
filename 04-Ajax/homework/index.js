d646a1ca0183ecd4194c2523a972560ad6a18342
cristhianleon22 <cristhiancleon@gmail.com>
2023-03-13 22:54:41 -0500

    04

---
+++ 04-Ajax/homework/index.js
@@ -0,0 +1,42 @@
+const myBtn = $("#boton");
+const list = $("#lista");
+
+myBtn.click(function (params) {
+    list.empty()
+    $.get("http://localhost:5000/amigos", (response) => {
+        response.forEach((element) => {
+            list.append(`<li>${element.id}: ${element.name} </li>`);
+        });
+    });
+});
+
+//get id
+
+const btnId = $("#search");
+const input = $("#input");
+btnId.click(() => {
+    const id = input.val();
+    console.log(id);
+    if (!isNaN(Number(id))) {
+        $.get(`http://localhost:5000/amigos/${id}`, (response) => {
+            console.log(response);
+            $("#amigo").text(response.name);
+        });
+    }
+});
+
+//delete id
+
+$("#delete").click(() => {
+    const id = $("#inputDelete").val();
+    if (!isNaN(Number(id))) {
+        $.ajax({
+            url:`http://localhost:5000/amigos/${id}`,
+            type:"DELETE",
+            success:()=>{
+                
+                $("#success").text("borrado con exito");
+            },
+        })
+    }
+});
